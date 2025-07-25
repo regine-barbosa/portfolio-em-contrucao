46cace925e6a81c5cd4973425cae95e5040772c3
regine-barbosa <60149787+regine-barbosa@users.noreply.github.com>
2025-07-25 04:35:31 -0300

    Create scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,33 @@
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement
+
+function changeTheme(){
+    const currentTheme = rootHtml.getAttribute("data-theme");
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+
+    toggleTheme.classList.toggle("bi-sun")
+    toggleTheme.classList.toggle("bi-moon")
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active")
+    })
+})
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+}
+)
